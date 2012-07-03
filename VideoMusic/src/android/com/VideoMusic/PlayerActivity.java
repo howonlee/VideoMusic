5224d11c29324a2499ce591a214ea0aa45c553e6
Howon Lee <hlee.howon@gmail.com>
2012-07-03 16:45:38 -0700

    camera preview now actually works

--- VideoMusic/src/android/com/VideoMusic/PlayerActivity.java
+++ VideoMusic/src/android/com/VideoMusic/PlayerActivity.java
@@ -12,6 +12,7 @@ public class PlayerActivity extends Activity implements SurfaceHolder.Callback {
 	Camera camera;
 	SurfaceView surfaceView;
 	SurfaceHolder surfaceHolder;
+	boolean isRunning;
 	int volume = 0;
 	
 	@Override
@@ -20,7 +21,17 @@ public class PlayerActivity extends Activity implements SurfaceHolder.Callback {
         setContentView(R.layout.player);
         surfaceView = (SurfaceView)findViewById(R.id.surface_view);
         surfaceHolder = surfaceView.getHolder();
-        camera = Camera.open();
+        surfaceHolder.addCallback(this);
+        //for 'compatibility'
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		//don't make any changes right now...
+	}
+
+	public void surfaceCreated(SurfaceHolder arg0) {
+		camera = Camera.open();
         if (camera != null){
         	try {
         		camera.setPreviewDisplay(surfaceHolder);
@@ -28,17 +39,7 @@ public class PlayerActivity extends Activity implements SurfaceHolder.Callback {
         	} catch (IOException e){
         		e.printStackTrace();
         	}
-        }
-    }
-
-	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void surfaceCreated(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
-		
+        }	
 	}
 
 	public void surfaceDestroyed(SurfaceHolder arg0) {
